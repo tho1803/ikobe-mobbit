7913420289463cf627c845f5318804f88b37ab22
Thomas <tho1803@users.noreply.github.com>
2026-03-28 13:00:30 +0100

    CI-Skill: Echtes Logo-Bild statt CSS-Balken, Farben aktualisiert

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,10 @@
+import logoLight from '../assets/ikobe-logo-light-bg.png';
+
 export default function Header() {
   return (
     <header className="app-header">
       <div className="ikobe-logo">
-        <div className="logo-bars">
-          <span className="logo-bar"></span>
-          <span className="logo-bar"></span>
-          <span className="logo-bar"></span>
-          <span className="logo-bar"></span>
-          <span className="logo-bar"></span>
-        </div>
-        IKOBE
+        <img src={logoLight} alt="IKOBE – Institut für Kompetenz und Begabung" className="logo-img" />
       </div>
       <span className="header-url">www.ikobe.de</span>
     </header>
